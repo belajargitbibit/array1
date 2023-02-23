2e53a24a0c9ac6457755925b4c956551ea829280
Bibit Kukuh Wicaksono <bibitkukuh00@gmail.com>
2023-02-23 16:33:31 +0700

    pertama

--- array.php
+++ array.php
@@ -1,9 +1,8 @@
 <?php
-// Belajar Array
 $hari = array("Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu");
-$hari1= "Selasa";
 // var_dump($hari);
-echo "Hari ini hari " .$hari[5]. "<br>";
-echo "Hari ini hari " .$hari1. "<br>";
+echo $hari[0];
+echo "<br>";
+print_r($hari[2]);
 
 ?>
