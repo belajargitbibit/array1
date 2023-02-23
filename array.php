ec0836343dfd2d3102fb5b7a21dc157f943cd2b7
Bibit Kukuh Wicaksono <bibitkukuh00@gmail.com>
2023-02-23 16:40:20 +0700

    for

--- array.php
+++ array.php
@@ -1,18 +1,30 @@
 <?php
-// cara lama
-$hari = array("Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu");
-// var_dump($hari);
-echo $hari[0];
-echo "<br>";
-print_r($hari[2]);
-echo "<br>";
-
-// cara baru
-$bulan = ["januari", "februari", "maret"];
-print_r($bulan);
-echo "<br>";
-print_r($hari);
-$hari[] = "kamis";
-echo "<br>";
-print_r($hari);
+$angka = [1, 3, 5, 7, 9];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Array</title>
+    <style> 
+        .kotak{
+            width: 50px;
+            height: 50px;
+            background-color: salmon;
+            text-align: center;
+            line-height: 50px;
+            margin: 30px;
+            float: left;
+        }
+    </style>
+</head>
+<body>
+    <?php for ($i = 0 ; $i < 5; $i++) {?>
+        <div class="kotak">
+            <?php echo $angka[$i]; ?>
+        </div>
+        <?php } ?>
+</body>
+</html>
