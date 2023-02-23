4fd81dae8ada0579d4e00814fd4b9e23197aa0de
Bibit Kukuh Wicaksono <bibitkukuh00@gmail.com>
2023-02-23 16:35:49 +0700

    kedua

--- array.php
+++ array.php
@@ -1,8 +1,13 @@
 <?php
+// cara lama
 $hari = array("Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu");
 // var_dump($hari);
 echo $hari[0];
 echo "<br>";
 print_r($hari[2]);
+echo "<br>";
 
+// cara baru
+$bulan = ["januari", "februari", "maret"];
+print_r($bulan);
 ?>
